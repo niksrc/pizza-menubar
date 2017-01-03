9ce4d2da5042ccea76295f8740608f64fc676bba
Nikhil Srivastava <niksrc@gmail.com>
2017-01-03 21:48:10 +0530

    add icon image

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const menubar = require('menubar');
-const app = menubar({ index: 'https://file.pizza', width: 575, height: 630 });
+
+const app = menubar({
+	index: 'https://file.pizza',
+	width: 575,
+	height: 630,
+	tooltip: 'File.Pizza',
+	icon: './IconTemplate2x.png'
+});
+
 app.on('ready', () => console.log('app is ready'));
 
